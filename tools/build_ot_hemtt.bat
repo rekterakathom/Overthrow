ef5e52358a8c06e3f05c8e18ddaba0c3aede5f4f
rekterakathom <30532050+rekterakathom@users.noreply.github.com>
2022-12-20 16:10:05 +0200

    Update build_ot_hemtt.bat
    Simplified the script and changed HEMTT location to P: drive

--- tools/build_ot_hemtt.bat
+++ tools/build_ot_hemtt.bat
@@ -6,7 +6,7 @@ ArmaScriptCompiler.exe
 
 :: HEMTT packing
 cd Overthrow
-start /wait "" "C:\Program Files (x86)\HEMTT\bin\hemtt.exe" build --release
+hemtt.exe build --release
 
 :: Cleanup .sqfc as it is not needed after being packed
 python sqfc_cleanup.py
