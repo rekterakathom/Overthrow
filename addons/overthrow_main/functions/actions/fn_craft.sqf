rekterakathom/Overthrow

commit 72e3ded53aaeef7b4c916afc82e98a4e985693da

Position updates
Changed: A lot of getPosxxx and position commands to more suitable equivalents or removed completely.

getPos is very inefficient and can lead to position errors.

addons/overthrow_main/functions/actions/fn_craft.sqf
@@ -13,7 +13,7 @@ if(count _def > 0) then {
     private _err = false;
     _def params ["_cls","_recipe","_qty"];
 
-    _container = getpos player nearestObject OT_item_Storage;
+    _container = player nearestObject OT_item_Storage;
     if !(isNull _container) then {
         if(_container distance player > 20) exitWith {"You need to be within 20m of an ammobox to craft" call OT_fnc_notifyMinor};
         _stock = _container call OT_fnc_unitStock;

commit d088eb6f61d88172df582c3874133b02b8d60c52

Fix crafting not working
Broken since 72e3ded53aaeef7b4c916afc82e98a4e985693da

addons/overthrow_main/functions/actions/fn_craft.sqf
@@ -13,7 +13,7 @@ if(count _def > 0) then {
     private _err = false;
     _def params ["_cls","_recipe","_qty"];
 
-    _container = player nearestObject OT_item_Storage;
+    _container = nearestObject [player, OT_item_Storage];
     if !(isNull _container) then {
         if(_container distance player > 20) exitWith {"You need to be within 20m of an ammobox to craft" call OT_fnc_notifyMinor};
         _stock = _container call OT_fnc_unitStock;