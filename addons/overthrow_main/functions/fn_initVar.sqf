rekterakathom/Overthrow

commit 5d95689a72230adaaf9c556ef14d243e3e86f1da

Optimize fn_initvar.sqf
Changed: Scope is checked only once for weapons, resulting in 2x faster execution

addons/overthrow_main/functions/fn_initVar.sqf
@@ -448,41 +448,17 @@ if(isServer) then {
 	OT_allVehicles pushBack _cls;
 }foreach(OT_vehicles);
 
-private _allWeapons = "
-    ( getNumber ( _x >> ""scope"" ) isEqualTo 2
-    &&
-    { getText ( _x >> ""simulation"" ) isEqualTo ""Weapon""})
-" configClasses ( _cfgWeapons );
+// Filter the scope only once
+private _filteredWeaponConfigs = "
+	(getNumber (_x >> 'scope') == 2)
+" configClasses (_cfgWeapons);
 
-private _allAttachments = "
-    ( getNumber ( _x >> ""scope"" ) isEqualTo 2
-    &&
-    { _t = getNumber ( _x >> ""ItemInfo"" >> ""type"" ); _t isEqualTo 301 || _t isEqualTo 302 || _t isEqualTo 101})
-" configClasses ( _cfgWeapons );
-
-private _allOptics = "
-    ( getNumber ( _x >> ""scope"" ) isEqualTo 2
-    &&
-    { getNumber ( _x >> ""ItemInfo"" >> ""optics"" ) isEqualTo 1})
-" configClasses ( _cfgWeapons );
-
-private _allDetonators = "
-    ( getNumber ( _x >> ""scope"" ) isEqualTo 2
-    &&
-    { getNumber ( _x >> ""ace_explosives_Detonator"" ) isEqualTo 1})
-" configClasses ( _cfgWeapons );
-
-private _allUniforms = "
-    ( getNumber ( _x >> ""scope"" ) isEqualTo 2
-    &&
-    { getNumber ( _x >> ""ItemInfo"" >> ""type"" ) isEqualTo 801})
-" configClasses ( _cfgWeapons );
-
-private _allHelmets = "
-    ( getNumber ( _x >> ""scope"" ) isEqualTo 2
-    &&
-    { getNumber ( _x >> ""ItemInfo"" >> ""type"" ) isEqualTo 605})
-" configClasses ( _cfgWeapons );
+// Could be optimized further, but this is already over twice as fast as the old method (65ms -> 25ms)
+private _allWeapons = _filteredWeaponConfigs select {getText ( _x >> "simulation" ) == "weapon"};
+private _allDetonators = _filteredWeaponConfigs select {getNumber ( _x >> "ace_explosives_Detonator" ) == 1};
+private _allAttachments = _filteredWeaponConfigs select {_t = getNumber ( _x >> "ItemInfo" >> "type" ); _t == 301 || _t == 302 || _t == 101};
+private _allUniforms = _filteredWeaponConfigs select {getNumber ( _x >> "ItemInfo" >> "type" ) == 801};
+private _allHelmets = _filteredWeaponConfigs select {getNumber ( _x >> "ItemInfo" >> "type" ) == 605};
 
 private _allAmmo = "
     ( getNumber ( _x >> ""scope"" ) isEqualTo 2 )

commit bf4c03f6947496807d215ef5c68ccc15a312dddb

Fix optics not processed in initVar
Broken since 5d95689a72230adaaf9c556ef14d243e3e86f1da

addons/overthrow_main/functions/fn_initVar.sqf
@@ -454,6 +454,7 @@ private _filteredWeaponConfigs = "
 private _allWeapons = [];
 private _allVests = [];
 private _allDetonators = [];
+private _allOptics = [];
 private _allAttachments = [];
 private _allUniforms = [];
 private _allHelmets = [];
@@ -461,6 +462,7 @@ private _allHelmets = [];
 {
 	if (getNumber (_x >> "type") in [1,2,4]) then { _allWeapons pushBack _x; continue};
 	if (getNumber (_x >> "ItemInfo">> "type") == 701) then { _allVests pushBack _x; continue};
+	if (getNumber (_x >> "ItemInfo" >> "type") == 201) then { _allOptics pushBack _x; continue };
 	if (getNumber (_x >> "ItemInfo" >> "type") in [101,301,302]) then { _allAttachments pushBack _x; continue };
 	if (getNumber (_x >> "ItemInfo" >> "type") == 605) then { _allHelmets pushBack _x; continue };
 	if (getNumber (_x >> "ItemInfo" >> "type") == 801) then { _allUniforms pushBack _x; continue };