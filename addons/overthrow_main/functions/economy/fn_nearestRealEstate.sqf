rekterakathom/Overthrow

commit efacf0b4aa5ee56c16aa868eab7025e91b0264f6

Replace BIS_fnc_arrayPushStack
Changed: Replaced BIS_fnc_arrayPushStack with either pushBack or append

addons/overthrow_main/functions/fn_initVar.sqf
@@ -995,7 +995,7 @@ OT_Buildables = [
 		private _tpl = _x select 2;
 		OT_allBuyableBuildings pushback ((_tpl select 0) select 0);
 	}else{
-		[OT_allBuyableBuildings,(_x select 2)] call BIS_fnc_arrayPushStack;
+		OT_allBuyableBuildings pushBack (_x select 2);
 	}
 }foreach(OT_Buildables);
 

addons/overthrow_main/functions/economy/fn_initEconomyLoad.sqf
@@ -153,13 +153,13 @@ if(_version < OT_economyVersion) then {
     };
 
 	_shops = server getVariable [format["activeshopsin%1",_x],[]];
-	[_allActiveShops,_shops] call BIS_fnc_arrayPushStack;
+	_allActiveShops append _shops;
 
 	_carshops = server getVariable [format["activecarshopsin%1",_x],[]];
-	[_allActiveCarShops,_carshops] call BIS_fnc_arrayPushStack;
+	_allActiveCarShops append _carshops;
 
 	_piers = server getVariable [format["activepiersin%1",_x],[]];
-	[_allActivePiers,_piers] call BIS_fnc_arrayPushStack;
+	_allActivePiers append _piers;
     sleep 0.3;
 }foreach(OT_allTowns);
 

addons/overthrow_main/functions/inventory/fn_getSearchStock.sqf
@@ -10,7 +10,7 @@ if(_this isKindOf "Man") then {
 }else{
 	_myitems = (itemCargo _this) + (weaponCargo _this) + (magazineCargo _this) + (backpackCargo _this);
 	{
-		_myitems = [_myitems,(items _this) + (magazines _this)] call BIS_fnc_arrayPushStack;
+		_myitems = _myitems append ((items _this) + (magazines _this));
 	}foreach(units _this);		
 };
 if !(isNil "_myitems") then {

addons/overthrow_main/functions/virtualization/spawners/fn_spawnCarDealers.sqf
@@ -21,7 +21,7 @@ private _groups = [];
 
 	_tracked = _building call OT_fnc_spawnTemplate;
 	_vehs = _tracked select 0;
-	[_groups,_vehs] call BIS_fnc_arrayPushStack;
+	_groups append _vehs;
 
 	_cashdesk = _pos nearestObject OT_item_ShopRegister;
 	_dir = getDir _cashdesk;

commit 4218f75f9ee4632dac906096b0a84e1c0787f2e3

Fix refugee camp not interactable
Bug introduced in commit efacf0b4. The building classes are a list, so it should be appended rather than pushBacked. This was also the reason why barracks didn't work lately, so I removed the previous barracks fix.

addons/overthrow_main/functions/economy/fn_nearestRealEstate.sqf
@@ -11,7 +11,7 @@ if(!isNil "modeTarget") then {
 };
 
 {
-	if ((typeof _x) in (OT_allBuyableBuildings + OT_allRepairableRuins + OT_warehouses + [OT_barracks])) exitWith {
+	if ((typeof _x) in (OT_allBuyableBuildings + OT_allRepairableRuins + OT_warehouses)) exitWith {
 		_building = _x;
 		_gotbuilding = true;
 	};

addons/overthrow_main/functions/fn_initVar.sqf
@@ -1015,7 +1015,7 @@ OT_Buildables = [
 		private _tpl = _x select 2;
 		OT_allBuyableBuildings pushback ((_tpl select 0) select 0);
 	}else{
-		OT_allBuyableBuildings pushBack (_x select 2);
+		OT_allBuyableBuildings append (_x select 2);
 	}
 }foreach(OT_Buildables);
 