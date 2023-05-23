rekterakathom/Overthrow

commit a9d3247031d169dfaf9bc42fbdb02df924cfba97

Separate warehouses
Changed: Warehouse inventories are now separated
Changed: Minor optimization

addons/overthrow_main/functions/actions/fn_recover.sqf
@@ -25,7 +25,7 @@ waitUntil {time > _end};
 
 //Get the loose weapons
 private _count_weapons = 0;
-private _weapons = _veh nearentities ["WeaponHolderSimulated",_range];
+private _weapons = _veh nearEntities ["WeaponHolderSimulated",_range];
 {
     _weapon = _x;
     _s = (weaponsItems _weapon) select 0;
@@ -48,9 +48,9 @@ private _weapons = _veh nearentities ["WeaponHolderSimulated",_range];
 private _count_bodies = 0;
 {
     if !((_x distance _veh > _range) || (alive _x)) then {
-        [_x,_veh] call OT_fnc_dumpStuff;
+        [_x, _veh] call OT_fnc_dumpStuff;
         _count_bodies = _count_bodies + 1;
-        deleteVehicle _x;
+        hideBody _x;
     };
 }foreach(entities "CAManBase");
 

addons/overthrow_main/CfgFunctions.hpp
@@ -346,6 +346,7 @@ class CfgFunctions
 			class getRandomRoadPosition {};
 			class isRadarInRange {};
 			class positionIsAtWarehouse {};
+			class nearestWarehouse {};
 		};
 
 		/*
@@ -442,6 +443,7 @@ class CfgFunctions
 			class findScopeInWarehouse {};
 			class findWeaponInWarehouse {};
 			class findVestInWarehouse {};
+			class verifyFromWarehouse {};
 			class verifyLoadoutFromWarehouse {};
 			class applyLoadoutFromWarehouse {};
 			class qtyInWarehouse {};
@@ -630,7 +632,7 @@ class CfgFunctions
 		class Integration
 		{
 			file = "\overthrow_main\functions\integration";
-			class advancedTowingInit {};
+			//class advancedTowingInit {};
 			class detectItems {};
 		};
 	};

addons/overthrow_main/campaign/missions/OverthrowMpAltis.Altis/description.ext
@@ -3,7 +3,7 @@
 overviewPicture = "\overthrow_main\campaign\missions\overthrow_tanoa.jpg";
 briefingName = "Overthrow: Altis";
 OnLoadName = "Overthrow: Altis";
-loadScreen = "\overthrow_main\campaign\missions\pic.jpg";
+loadScreen = "\overthrow_main\campaign\RKT-loadingscreen.jpg";
 
 overviewText = "Altis, the main island of the Republic of Altis and Stratis, is located in the Mediterranean Sea to the east of Malta. As of 2040, Altis is currently under occupation by NATO forces following a brutal civil war a half decade prior, and the nation is still recovering. NATO originally occupied the country under the promise of a complete withdrawal of forces and return to local democratic rule; despite this, NATO soldiers still occupy the island's military installations. The economy continues to stagnate and citizens are becoming increasingly angry at the lack of local autonomy.";
 

addons/overthrow_main/campaign/missions/OverthrowMpLivonia.Enoch/description.ext
@@ -3,7 +3,7 @@
 overviewPicture = "\overthrow_main\campaign\missions\overthrow_tanoa.jpg";
 briefingName = "Overthrow: Livonia";
 OnLoadName = "Overthrow: Livonia";
-loadScreen = "\overthrow_main\campaign\missions\pic.jpg";
+loadScreen = "\overthrow_main\campaign\RKT-loadingscreen.jpg";
 
 overviewText = "TBC";
 

addons/overthrow_main/campaign/missions/OverthrowMpMalden.Malden/description.ext
@@ -3,7 +3,7 @@
 overviewPicture = "\overthrow_main\campaign\missions\overthrow_tanoa.jpg";
 briefingName = "Overthrow: Malden";
 OnLoadName = "Overthrow: Malden";
-loadScreen = "\overthrow_main\campaign\missions\pic.jpg";
+loadScreen = "\overthrow_main\campaign\RKT-loadingscreen.jpg";
 
 overviewText = "@todo";
 

addons/overthrow_main/campaign/missions/OverthrowMpTanoa.Tanoa/description.ext
@@ -3,7 +3,7 @@
 overviewPicture = "\overthrow_main\campaign\missions\overthrow_tanoa.jpg";
 briefingName = "Overthrow: Tanoa";
 OnLoadName = "Overthrow: Tanoa";
-loadScreen = "\overthrow_main\campaign\missions\pic.jpg";
+loadScreen = "\overthrow_main\campaign\RKT-loadingscreen.jpg";
 
 overviewText = "This is Tanoa, a small island nation in the South Pacific with a history of British and French colonialism. Tanoa is currently under occupation by NATO forces and has been at the center of international attention since the Vunakece regime was ousted in 2020. The Tanoan population is currently in disarray and there are many conspiracy theories as to the economic and political future of this small but strategically valuable archipelago. A massive protest against the NATO occupation recently took place in the nation's capital of Georgetown, where the spokesperson for the 'Free Tanoa' movement was assassinated by an unknown assailant. In response, NATO forces have issued a strict curfew and increased security to extreme levels; fuelling tension and conspiracy theories even further.";
 

addons/overthrow_main/campaign/missions/OverthrowSpAltis.Altis/description.ext
@@ -3,7 +3,7 @@
 overviewPicture = "\overthrow_main\campaign\missions\overthrow_tanoa.jpg";
 briefingName = "Overthrow: Altis";
 OnLoadName = "Overthrow: Altis";
-loadScreen = "\overthrow_main\campaign\missions\pic.jpg";
+loadScreen = "\overthrow_main\campaign\RKT-loadingscreen.jpg";
 
 overviewText = "Altis, the main island of the Republic of Altis and Stratis, is located in the Mediterranean Sea to the east of Malta. As of 2040, Altis is currently under occupation by NATO forces following a brutal civil war a half decade prior, and the nation is still recovering. NATO originally occupied the country under the promise of a complete withdrawal of forces and return to local democratic rule; despite this, NATO soldiers still occupy the island's military installations. The economy continues to stagnate and citizens are becoming increasingly angry at the lack of local autonomy.";
 

addons/overthrow_main/campaign/missions/OverthrowSpMalden.Malden/description.ext
@@ -3,7 +3,7 @@
 overviewPicture = "\overthrow_main\campaign\missions\overthrow_tanoa.jpg";
 briefingName = "Overthrow: Malden";
 OnLoadName = "Overthrow: Malden";
-loadScreen = "\overthrow_main\campaign\missions\pic.jpg";
+loadScreen = "\overthrow_main\campaign\RKT-loadingscreen.jpg";
 
 overviewText = "@todo";
 

commit 2a86bcd9e4ba9d27630f4e672163ce5efcdf6f08

Fix looted bodies not getting removed.
Bug introduced in a9d3247. hideBody requires a local argument, but looted bodies may be local to any machine, usually the server. This caused bodies to disappear only for some players. Furthermore, hideBody deletes bodies slowly, so the death icons are still visible on the map quite long after the looting.

addons/overthrow_main/functions/actions/fn_recover.sqf
@@ -50,7 +50,7 @@ private _count_bodies = 0;
     if !((_x distance _veh > _range) || (alive _x)) then {
         [_x, _veh] call OT_fnc_dumpStuff;
         _count_bodies = _count_bodies + 1;
-        hideBody _x;
+        [_x] call OT_fnc_cleanupUnit;
     };
 }foreach(entities "CAManBase");
 